b080923b8dfad21ded0d80c1308da2fdc7ca4f8d
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-05-11 12:50:04 +0900

    Update

--- demo1/main.go
+++ demo1/main.go
@@ -5,11 +5,24 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf(
+		`{"timestamp":"%s","method":"%s","url":"%s","remote":"%s"}`+"\n",
+		time.Now().UTC().Format(time.RFC3339),
+		r.Method,
+		r.URL,
+		r.RemoteAddr,
+	)
 	magicWord := os.Getenv("MAGIC_WORD")
 	fmt.Fprintf(w, "The magic word is %q", magicWord)
+}
+
+func init() {
+	log.SetFlags(0)
+	log.SetPrefix("")
 }
 
 func main() {
